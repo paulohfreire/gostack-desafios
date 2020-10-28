4add1c19f5833e0f082ba34c5e24ae816eede76d
Paulohfreire <paulo.freire53@gmail.com>
2020-10-28 00:50:58 -0300

    add-explanation-text-no-code

--- src/app.js
+++ src/app.js
@@ -9,10 +9,12 @@ app.use(cors());
 
 const repositories = [];
 
+//Listar os repositórios, retonando um Json com a lista de repositories
 app.get('/repositories', (request, response) => {
   return response.json(repositories);
 });
 
+//Criar os repositórios, iniciando com 0 de likes. Comando push envia individualmente e retorna em formato json individualmente, também
 app.post('/repositories', (request, response) => {
   const { title, url, techs, likes} = request.body;
   const repository = { id: uuid(), title, url, techs, likes: 0 };
@@ -21,6 +23,11 @@ app.post('/repositories', (request, response) => {
 
   return response.json(repository);
 });
+
+//Para editar um repositório específico - por isso o :id, podendo editar as variáveis da const ... request.body
+//const repositorioIndex é para encontrar o id do repositório
+//se o id for menor que 0 significa que o repositório não existe
+
 
 app.put('/repositories/:id', (request, response) => {
   const { id } = request.params;
@@ -47,6 +54,10 @@ app.put('/repositories/:id', (request, response) => {
   return response.json(repository);
 });
 
+//Para deletar um repositório, localiza o id, depois verifica se ele existe.
+//se for >= 0 o id, o repositório existe e para deletar é utilizado o comando splice
+//O return do final é para listar vazio para mostrar que o repositório foi mesmo apagado.
+
 app.delete('/repositories/:id', (request, response) => {
   const { id } = request.params;
   const repositoryIndex = repositories.findIndex(
@@ -59,6 +70,10 @@ app.delete('/repositories/:id', (request, response) => {
   }
   return response.status(204).send();
 });
+
+//Para editar a função de likes, que não poderia ser editada manualmente
+//verifica o id, depois se ele existe
+//utiliza o repositories[repositoryIndex].likes++; para adicionar um like
 
 app.post('/repositories/:id/like', (request, response) => {
   const { id } = request.params;
